d83d1ad91bb11285110860359f0bf90ffde40396
Cláudio Gil <claudio@m4ktub.ws>
2024-09-08 12:14:01 +0100

    Check for TS errors before esbuild

--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -1,7 +1,36 @@
-import * as esbuild from 'esbuild';
 import { globalExternals } from "@fal-works/esbuild-plugin-global-externals";
+import * as esbuild from 'esbuild';
+import os from 'os';
+import ts from 'typescript';
 
 const entry = process.argv[2];
+
+//
+// check types through TypeScript compilation
+//
+
+const program = ts.createProgram([ entry ], {
+  noEmit: true,
+  esModuleInterop: true,
+  module: ts.ModuleKind.CommonJS,
+  target: ts.ScriptTarget.ES2016
+});
+
+const diagnostics = ts.getPreEmitDiagnostics(program);
+if (diagnostics.length > 0) {
+  const diagnosticsText = ts.formatDiagnosticsWithColorAndContext(diagnostics, {
+    getCanonicalFileName: (file) => file,
+    getNewLine: () => os.EOL,
+    getCurrentDirectory: () => process.cwd(),
+  });
+
+  console.log(diagnosticsText);
+  process.exit(1);
+}
+
+//
+// produce JS bundle with esbuild
+//
 
 const globals = {
   jquery: "$"
